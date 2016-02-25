1183022185c95d381c707f5b824b57df120479a0
szymonbaranczyk <szymonbaranczyk@gmail.com>
2016-02-25 17:11:35 +0100

    Small clean up

--- src/main/java/zpi/hotelweb/controllers/UserController.java
+++ src/main/java/zpi/hotelweb/controllers/UserController.java
@@ -1,7 +1,10 @@
 package zpi.hotelweb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +18,6 @@ import java.util.Map;
  * Created by User1 on 25/02/2016.
  */
 @Controller
-@RequestMapping("user")
 public class UserController {
 
 
@@ -23,11 +25,21 @@ public class UserController {
     UserService userService;
 
     @ResponseBody
-    @RequestMapping(value = "/loggedUserDetails", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/loggedUserDetails", method = RequestMethod.GET)
     public Map<String, Object> getLoggedUserDetails() {
         User user = userService.getLoggedUser();
         Map<String, Object> returned = new HashMap<>();
         returned.put("username", user.getUsername());                //dodac więcej szczegółów
         return returned;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<String> register(@RequestBody User user) {
+        try {
+            userService.register(user);
+            return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
